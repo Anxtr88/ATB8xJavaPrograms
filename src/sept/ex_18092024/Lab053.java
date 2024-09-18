206d424119861aa054a05c253f2f174bfc2ceb2e
Anxtr88 <mendo1812@gmail.com>
2024-09-18 08:25:09 +0530

    done

---
+++ src/sept/ex_18092024/Lab053.java
@@ -0,0 +1,12 @@
+package sept.ex_18092024;
+
+public class Lab053 {
+
+    public static void main(String[] args) {
+      // increment / Decrement operators
+        int a = 10;
+        System.out.println(a);
+
+
+    }
+}

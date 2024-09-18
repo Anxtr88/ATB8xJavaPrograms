76344c29d27af1fbcfd1a61abcd98937ff6922fb
Anxtr88 <mendo1812@gmail.com>
2024-09-18 22:13:03 +0530

    All file 11 Sept

---
+++ src/sept/ex_11092024/Lab012.java
@@ -0,0 +1,25 @@
+package sept.ex_11092024;
+
+public class Lab012 {
+
+    public static void main(String[] args) {
+        //Primitive Data Type
+        boolean age = true;
+        byte b = 100; // 8 bits, 1 Byte
+        short s = 128; // 16 bits, 2 byte
+        int i = 1000; // 32 bits, 4 byte
+        long ln = 4363464539L; // 64 bits, 8 bytes
+
+        char grade = 'A' ; //16 bits, 2 byte
+
+        float f = 3.45f; // 32 bits, 4 bytes
+
+        double d =224.2233342; // 64 bits, 8 bytes
+
+
+        //Non Primitive Data type
+        String name = "Promode";
+
+
+    }
+}
---
+++ src/sept/ex_11092024/Lab053.java
@@ -0,0 +1,26 @@
+package sept.ex_11092024;
+
+import java.util.Arrays;
+
+public class Lab053 {
+
+    public static void main(String[] args) {
+
+        char c = 'A';
+        System.out.println(c);
+
+        char c2 = '\n';  // new line escape char
+        System.out.println(c2);
+
+        char c3 = '$';
+        System.out.println(c3);
+
+        char c4 = '\u1F6A';  // Unicode
+        System.out.println(c4);
+
+
+
+
+
+    }
+}

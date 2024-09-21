bdb43a6838eaae74c88101aab4d0ddebc3202caa
Anxtr88 <mendo1812@gmail.com>
2024-09-21 09:12:27 +0530

    All file Sept

---
+++ src/sept/ex_16092024/Lab029_Operators.java
@@ -0,0 +1,19 @@
+package sept.ex_16092024;
+
+import java.util.Arrays;
+
+public class Lab029_Operators {
+
+    public static void main(String[] args) {
+        String name = "Promode";
+        String lname = "Dutta";
+
+        System.out.println(name + lname + 10 + 20);
+        System.out.println(10 + 20 + name + lname + 10 + 20);
+
+
+
+    }
+
+    
+}
